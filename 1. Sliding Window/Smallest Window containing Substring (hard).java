e25a57763ea117d22f21d6f50f46c99579eaf859
Edu Roy <1610132@mail.nankai.edu.cn>
2020-09-29 11:10:29 +0800

    challenge three finished hard problems

---
+++ 1. Sliding Window/Smallest Window containing Substring (hard).java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+class MinimumWindowSubstring {
+  public static String findSubstring(String str, String pattern) {
+    int windowStart = 0, matched = 0, minLength = str.length() + 1, subStrStart = 0;
+    Map<Character, Integer> charFrequencyMap = new HashMap<>();
+    for (char chr : pattern.toCharArray())
+      charFrequencyMap.put(chr, charFrequencyMap.getOrDefault(chr, 0) + 1);
+
+    // try to extend the range [windowStart, windowEnd]
+    for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+      char rightChar = str.charAt(windowEnd);
+      if (charFrequencyMap.containsKey(rightChar)) {
+        charFrequencyMap.put(rightChar, charFrequencyMap.get(rightChar) - 1);
+        if (charFrequencyMap.get(rightChar) >= 0) // count every matching of a character
+          matched++;
+      }
+
+      // shrink the window if we can, finish as soon as we remove a matched character
+      while (matched == pattern.length()) {
+        if (minLength > windowEnd - windowStart + 1) {
+          minLength = windowEnd - windowStart + 1;
+          subStrStart = windowStart;
+        }
+
+        char leftChar = str.charAt(windowStart++);
+        if (charFrequencyMap.containsKey(leftChar)) {
+          // note that we could have redundant matching characters, therefore we'll decrement the
+          // matched count only when a useful occurrence of a matched character is going out of the window
+          if (charFrequencyMap.get(leftChar) == 0)
+            matched--;
+          charFrequencyMap.put(leftChar, charFrequencyMap.get(leftChar) + 1);
+        }
+      }
+    }
+
+    return minLength > str.length() ? "" : str.substring(subStrStart, subStrStart + minLength);
+  }
+
+  public static void main(String[] args) {
+    System.out.println(MinimumWindowSubstring.findSubstring("aabdec", "abc"));
+    System.out.println(MinimumWindowSubstring.findSubstring("abdbca", "abc"));
+    System.out.println(MinimumWindowSubstring.findSubstring("adcad", "abc"));
+  }
+}
--- 1. Sliding Window/String Anagrams (hard).java
+++ 1. Sliding Window/String Anagrams (hard).java
@@ -12,8 +12,28 @@ class StringAnagrams {
 
     List<Integer> resultIndices = new ArrayList<Integer>();
 
-    
+    for(int windowEnd = 0; windowEnd < str.length(); windowEnd++){
+    	char rightChar = str.charAt(windowEnd);
 
+    	if(charFrequencyMap.containsKey(rightChar)){
+    		charFrequencyMap.put(rightChar, charFrequencyMap.get(rightChar) - 1);
+    		if(charFrequencyMap.get(rightChar) == 0)
+    			matched++;
+    	}
 
+    	if(matched == charFrequencyMap.size())
+    		resultIndices.add(windowStart);
+
+    	if(windowEnd >= pattern.length() - 1){
+    		char leftChar = str.charAt(windowStart++);
+    		if(charFrequencyMap.get(leftChar) == 0)
+    			matched--;
+
+    		charFrequencyMap.put(leftChar, charFrequencyMap.get(leftChar) - 1);
+    	}
+
+    }
+
+    return resultIndices;
   }
 }
