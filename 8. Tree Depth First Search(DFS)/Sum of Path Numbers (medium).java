44f054e1fa60bcd5df780532d15cf2aaf04aa8e2
Edu Roy <1610132@mail.nankai.edu.cn>
2020-10-11 19:49:58 +0800

    sum of path numbers

---
+++ 8. Tree Depth First Search(DFS)/Sum of Path Numbers (medium).java
@@ -0,0 +1,41 @@
+import java.util.*;
+
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode(int x) {
+    val = x;
+  }
+};
+
+class SumOfPathNumbers {
+  public static int findSumOfPathNumbers(TreeNode root) {
+    return findRootToLeafPathNumbers(root, 0);
+  }
+
+  private static int findRootToLeafPathNumbers(TreeNode currentNode, int pathSum){
+    if(currentNode == null)
+      return 0;
+
+    pathSum = 10*pathSum + currentNode;
+
+    if(currentNode.left == null && currentNode.right == null){
+      return pathSum;
+    }
+
+    return findRootToLeafPathNumbers(currentNode.left, pathSum) + 
+          findRootToLeafPathNumbers(currentNode.right, pathSum);
+  }
+
+  public static void main(String[] args) {
+    TreeNode root = new TreeNode(1);
+    root.left = new TreeNode(0);
+    root.right = new TreeNode(1);
+    root.left.left = new TreeNode(1);
+    root.right.left = new TreeNode(6);
+    root.right.right = new TreeNode(5);
+    System.out.println("Total Sum of Path Numbers: " + SumOfPathNumbers.findSumOfPathNumbers(root));
+  }
+}
