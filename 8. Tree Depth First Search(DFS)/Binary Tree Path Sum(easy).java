19efcda730432b3d6597ab58d100b626ccdc4b61
Edu Roy <1610132@mail.nankai.edu.cn>
2020-10-11 19:43:31 +0800

    all the path sum

---
+++ 8. Tree Depth First Search(DFS)/Binary Tree Path Sum(easy).java
@@ -0,0 +1,36 @@
+
+class TreeNode {
+  int val;
+  TreeNode left;
+  TreeNode right;
+
+  TreeNode(int x) {
+    val = x;
+  }
+};
+
+class TreePathSum {
+  public static boolean hasPath(TreeNode root, int sum) {
+    if(root == null)
+      return false;
+
+    // if the current node is a leaf and its value is equal to the sum, we've found a path
+    if (root.val == sum && root.left == null && root.right == null)
+      return true;
+
+    // recursively call to traverse the left and right sub-tree
+    // return true if any of the two recursive call return true
+    return hasPath(root.left, sum - root.val) || hasPath(root.right, sum - root.val);
+  }
+
+  public static void main(String[] args) {
+    TreeNode root = new TreeNode(12);
+    root.left = new TreeNode(7);
+    root.right = new TreeNode(1);
+    root.left.left = new TreeNode(9);
+    root.right.left = new TreeNode(10);
+    root.right.right = new TreeNode(5);
+    System.out.println("Tree has path: " + TreePathSum.hasPath(root, 23));
+    System.out.println("Tree has path: " + TreePathSum.hasPath(root, 16));
+  }
+}
