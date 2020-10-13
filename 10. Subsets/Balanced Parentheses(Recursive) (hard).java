7ee1efc265b9e4ea16ffc175ee36550736189a82
Edu Roy <1610132@mail.nankai.edu.cn>
2020-10-13 20:13:01 +0800

    exactly balanced parentheses using recursive but not understood

---
+++ 10. Subsets/Balanced Parentheses(Recursive) (hard).java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+class GenerateParenthesesRecursive {
+
+  public static List<String> generateValidParentheses(int num) {
+    List<String> result = new ArrayList<String>();
+    char[] parenthesesString = new char[2 * num];
+    generateValidParenthesesRecursive(num, 0, 0, parenthesesString, 0, result);
+    return result;
+  }
+
+  private static void generateValidParenthesesRecursive(int num, int openCount, int closeCount,
+      char[] parenthesesString, int index, List<String> result) {
+
+    // if we've reached the maximum number of open and close parentheses, add to the result
+    if (openCount == num && closeCount == num) {
+      result.add(new String(parenthesesString));
+    } else {
+      if (openCount < num) { // if we can add an open parentheses, add it
+        parenthesesString[index] = '(';
+        generateValidParenthesesRecursive(num, openCount + 1, closeCount, parenthesesString, index + 1, result);
+      }
+
+      if (openCount > closeCount) { // if we can add a close parentheses, add it
+        parenthesesString[index] = ')';
+        generateValidParenthesesRecursive(num, openCount, closeCount + 1, parenthesesString, index + 1, result);
+      }
+    }
+  }
+
+  public static void main(String[] args) {
+    List<String> result = GenerateParenthesesRecursive.generateValidParentheses(2);
+    System.out.println("All combinations of balanced parentheses are: " + result);
+
+    result = GenerateParenthesesRecursive.generateValidParentheses(3);
+    System.out.println("All combinations of balanced parentheses are: " + result);
+  }
+}
