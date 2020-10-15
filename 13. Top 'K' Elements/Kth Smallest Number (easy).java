d064c40aebbc76db3e42bbdb11fafe9ad5ae52cb
Edu Roy <1610132@mail.nankai.edu.cn>
2020-10-15 15:57:39 +0800

    problem 2 and 3 finished

---
+++ 13. Top 'K' Elements/Kth Smallest Number (easy).java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class KthSmallestNumber {
+
+  public static int findKthSmallestNumber(int[] nums, int k) {
+    PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>((n1, n2) -> n2 - n1);
+    // put first k numbers in the max heap
+    for (int i = 0; i < k; i++)
+      maxHeap.add(nums[i]);
+
+    // go through the remaining numbers of the array, if the number from the array is smaller than the
+    // top (biggest) number of the heap, remove the top number from heap and add the number from array
+    for (int i = k; i < nums.length; i++) {
+      if (nums[i] < maxHeap.peek()) {
+        maxHeap.poll();
+        maxHeap.add(nums[i]);
+      }
+    }
+
+    // the root of the heap has the Kth smallest number
+    return maxHeap.peek();
+  }
+
+  public static void main(String[] args) {
+    int result = KthSmallestNumber.findKthSmallestNumber(new int[] { 1, 5, 12, 2, 11, 5 }, 3);
+    System.out.println("Kth smallest number is: " + result);
+
+    // since there are two 5s in the input array, our 3rd and 4th smallest numbers should be a '5'
+    result = KthSmallestNumber.findKthSmallestNumber(new int[] { 1, 5, 12, 2, 11, 5 }, 4);
+    System.out.println("Kth smallest number is: " + result);
+
+    result = KthSmallestNumber.findKthSmallestNumber(new int[] { 5, 12, 11, -1, 12 }, 3);
+    System.out.println("Kth smallest number is: " + result);  }
+}
---
+++ 13. Top 'K' Elements/Top 'K' Numbers(easy).java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+class KLargestNumbers {
+
+  public static List<Integer> findKLargestNumbers(int[] nums, int k) {
+    PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>((n1, n2) -> n1 - n2);
+    // put first 'K' numbers in the min heap
+    for (int i = 0; i < k; i++)
+      minHeap.add(nums[i]);
+
+    // go through the remaining numbers of the array, if the number from the array is bigger than the
+    // top (smallest) number of the min-heap, remove the top number from heap and add the number from array
+    for (int i = k; i < nums.length; i++) {
+      if (nums[i] > minHeap.peek()) {
+        minHeap.poll();
+        minHeap.add(nums[i]);
+      }
+    }
+
+    // the heap has the top 'K' numbers, return them in a list
+    return new ArrayList<>(minHeap);
+  }
+
+  public static void main(String[] args) {
+    List<Integer> result = KLargestNumbers.findKLargestNumbers(new int[] { 3, 1, 5, 12, 2, 11 }, 3);
+    System.out.println("Here are the top K numbers: " + result);
+
+    result = KLargestNumbers.findKLargestNumbers(new int[] { 5, 12, 11, -1, 12 }, 3);
+    System.out.println("Here are the top K numbers: " + result);
+  }
+}
