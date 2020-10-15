461bfc72ce2c8610a751161dba15d72a8913c676
Edu Roy <1610132@mail.nankai.edu.cn>
2020-10-15 10:49:46 +0800

    finally get through the XOR in single two

---
+++ 12. Bitwise XOR/Two Single Numbers (easy).java
@@ -0,0 +1,34 @@
+class TwoSingleNumbers {
+
+  public static int[] findSingleNumbers(int[] nums) {
+    // get the XOR of the all the numbers
+    int n1xn2 = 0;
+    for (int num : nums) {
+      n1xn2 ^= num;
+    }
+
+    // get the rightmost bit that is '1'
+    int rightmostSetBit = 1;
+    while ((rightmostSetBit & n1xn2) == 0) {
+      rightmostSetBit = rightmostSetBit << 1;
+    }
+    int num1 = 0, num2 = 0;
+    for (int num : nums) {
+      if ((num & rightmostSetBit) != 0) // the bit is set
+        num1 ^= num;
+      else // the bit is not set
+        num2 ^= num;
+    }
+    return new int[] { num1, num2 };
+  }
+
+  public static void main(String[] args) {
+    int[] arr = new int[] { 1, 4, 2, 1, 3, 5, 6, 2, 3, 5 };
+    int[] result = TwoSingleNumbers.findSingleNumbers(arr);
+    System.out.println("Single numbers are: " + result[0] + ", " + result[1]);
+
+    arr = new int[] { 2, 1, 3, 2 };
+    result = TwoSingleNumbers.findSingleNumbers(arr);
+    System.out.println("Single numbers are: " + result[0] + ", " + result[1]);
+  }
+}
